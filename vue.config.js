645255b2d5a88e0e1179504872e7b71b0e4ede39
ChenXiaohui <753480511@qq.com>
2022-01-28 11:25:32 +0800

    chore: 优化vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,8 @@ const Icons = require('unplugin-icons/webpack')
 const IconsResolver = require('unplugin-icons/resolver')
 const ExtensionReloader = require('webpack-extension-reloader')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   // vue-cli-plugin-browser-extension 相关设置
   pages: {
@@ -12,14 +14,14 @@ module.exports = {
       template: 'public/browser-extension.html',
       entry: './src/popup/main.js',
       title: 'Popup',
-    }
+    },
   },
   pluginOptions: {
     browserExtension: {
       manifestSync: ['version'],
       components: {
         background: true,
-        contentScripts: true
+        contentScripts: true,
       },
       componentOptions: {
         background: {
@@ -33,32 +35,42 @@ module.exports = {
       },
     },
   },
-  configureWebpack: {
-    plugins: [
-      // 自动导入Element Plus 以及 Element Icon
+  productionSourceMap: isProduction ? false : true,
+  css: {
+    // 打包提示警告信息:warning Conflicting order
+    // see https://github.com/vuejs/vue-cli/issues/3771#issuecomment-593360794
+    extract: isProduction ? { ignoreOrder: true } : false,
+  },
+  configureWebpack: (config) => {
+    // 自动导入Element Plus 以及 Element Icon
+    config.plugins.push(
       AutoImport({
         resolvers: [ElementPlusResolver()],
       }),
       Components({
         resolvers: [ElementPlusResolver(), IconsResolver()],
       }),
-      Icons(),
-      // webpack-extension-reloader 热加载端口设置
-      new ExtensionReloader({
-        port: 9091
-      })
-    ],
+      Icons()
+    )
+    if (isProduction) {
+      // webpack-extension-reloader 热加载
+      config.plugins.push(
+        new ExtensionReloader({
+          port: 9091,
+        })
+      )
+    }
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // 自定义的 Icon-svg 组件配置
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
     svgRule
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]",
-        include: ["./src/svg"]
-      });
-  }
+        symbolId: 'icon-[name]',
+        include: ['./src/svg'],
+      })
+  },
 }
