0906070e4aa6895357e21db198bc296499845f06
刘绪 <1427868463@qq.com>
2025-05-24 10:51:24 +0800

    feat(api): 添加文章详情和互动相关的 API 接口
    - 新增获取文章详情接口
    - 新增文章点赞、感谢接口
    - 新增文章评论列表接口
    - 新增发表、点赞、感谢、删除评论的接口

--- src/popup/api/article.js
+++ src/popup/api/article.js
@@ -35,3 +35,75 @@ export function getReplyArticles(params) {
     params
   })
 }
+
+// 获取指定帖子详情
+export function getArticleDetail(articleId, params) {
+  return request({
+    url: `/api/article/${articleId}`,
+    method: 'get',
+    params
+  })
+}
+
+// 给文章点赞
+export function voteUpArticle(data) {
+  return request({
+    url: '/vote/up/article',
+    method: 'post',
+    data
+  })
+}
+
+// 感谢文章
+export function thankArticle(articleId, data) {
+  return request({
+    url: `/article/thank?articleId=${articleId}`,
+    method: 'post',
+    data
+  })
+}
+
+// 获取文章评论列表
+export function getArticleComments(articleId, params) {
+  return request({
+    url: `/api/comment/${articleId}`,
+    method: 'get',
+    params
+  })
+}
+
+// 发表评论
+export function addComment(data) {
+  return request({
+    url: '/comment',
+    method: 'post',
+    data
+  })
+}
+
+// 给评论点赞
+export function voteUpComment(data) {
+  return request({
+    url: '/vote/up/comment',
+    method: 'post',
+    data
+  })
+}
+
+// 感谢评论
+export function thankComment(data) {
+  return request({
+    url: '/comment/thank',
+    method: 'post',
+    data
+  })
+}
+
+// 删除评论
+export function removeComment(commentId, data) {
+  return request({
+    url: `/comment/${commentId}/remove`,
+    method: 'post',
+    data
+  })
+}
